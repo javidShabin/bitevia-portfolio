486dbb09719d9021f37a72fad9cb1d045c1004a1
javid shabin <javid.main.act@gmail.com>
2025-09-24 15:55:34 +0530

    Add some content in the project page

--- src/components/ui/CardSwap.tsx
+++ src/components/ui/CardSwap.tsx
@@ -36,7 +36,7 @@ export const Card = forwardRef<HTMLDivElement, CardProps>(
     <div
       ref={ref}
       {...rest}
-      className={`absolute top-1/2 left-1/2 rounded-xl border border-white bg-black [transform-style:preserve-3d] [will-change:transform] [backface-visibility:hidden] ${customClass ?? ""} ${
+      className={`absolute top-1/5 left-1/3 rounded-xl border border-white bg-black [transform-style:preserve-3d] [will-change:transform] [backface-visibility:hidden] ${customClass ?? ""} ${
         rest.className ?? ""
       }`.trim()}
     />
